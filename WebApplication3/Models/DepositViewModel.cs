01952a1124740bdd8f543227e1d17885d7e348ec
seph <148517529+Capungan-JohnJoseph-BSIT32E3@users.noreply.github.com>
2024-06-20 14:36:57 +0800

    Commit DepositViewModel

---
+++ WebApplication3/Models/DepositViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication3.ViewModels
+{
+    public class DepositViewModel
+    {
+        public string AccountNumber { get; set; }
+        public decimal Amount { get; set; }
+        public string TransactionType { get; set; } = "Deposit"; 
+    }
+}

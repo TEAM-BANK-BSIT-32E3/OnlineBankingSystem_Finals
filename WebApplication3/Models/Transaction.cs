3698cb91d47ffa0d4a64cb185a08d6192a2658a8
Kervy Lagustan -BSIT32E3 <167051276+YeojgeCo@users.noreply.github.com>
2024-06-20 23:43:25 +0800

    Create Transaction.cs
    creating transactions model

---
+++ WebApplication3/Models/Transaction.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication3.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string AccountName { get; set; }
+
+        [Required]
+        public string AccountNumber { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string TransactionType { get; set; }
+
+        [Required]
+        [Range(0, Double.MaxValue)]
+        public decimal Amount { get; set; }
+
+        public DateTime TransactionDate { get; set; } = DateTime.Now;
+
+        public string Description { get; set; }
+
+        // Add additional properties for money requests
+        public string RecipientAccountNumber { get; set; } // This is the account number of the recipient
+        public string RequestStatus { get; set; } // Status of the money request (e.g., pending, accepted, rejected)
+
+
+    }
+}

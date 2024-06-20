fbc5ead572e4feeec94159c6897cab6d98c45051
Michael <Michael@LAPTOP-8EN5ERDG>
2024-06-20 13:28:42 +0800

    Commit RequestMoneyViewModel

---
+++ WebApplication3/Models/RequestMoneyViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication3.Models
+{
+    public class RequestMoneyViewModel
+    {
+        public decimal Amount { get; set; }
+        public string Description { get; set; }
+        public string RecipientAccountNumber { get; set; } // This will capture the recipient's account number
+    }
+
+}

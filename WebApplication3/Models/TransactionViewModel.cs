cc32935409e3a2577ba2c516a41301948cf5d349
Kervy Lagustan -BSIT32E3 <167051276+YeojgeCo@users.noreply.github.com>
2024-06-20 23:43:40 +0800

    Create TransactionViewModel.cs
    Creating transaction view model

---
+++ WebApplication3/Models/TransactionViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication3.ViewModels
+{
+    public class TransactionViewModel
+    {
+        public string AccountName { get; set; }
+        public string AccountNumber { get; set; }
+        public decimal Amount { get; set; }
+        public string TransactionType { get; set; }
+        public string Description { get; set; }
+
+
+
+    }
+}

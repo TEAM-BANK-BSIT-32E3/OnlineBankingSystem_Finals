83aedbb557b1486ab4d4d67e142fb945632a1bac
Kervy Lagustan -BSIT32E3 <kervysmith39@gmail.com>
2024-06-20 23:39:42 +0800

    Create 20240612053736_AddAccountNumberToUser.cs

---
+++ WebApplication3/Migrations/20240612053736_AddAccountNumberToUser.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication3.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAccountNumberToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AccountNumber",
+                table: "Users",
+                type: "TEXT",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AccountNumber",
+                table: "Users");
+        }
+    }
+}

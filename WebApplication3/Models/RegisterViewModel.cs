877f960b8cfe82ef59f950c98ce5b6832920238f
paaatikk <161110953+paaatikk@users.noreply.github.com>
2024-06-20 13:11:33 +0800

    Commit Register Model

---
+++ WebApplication3/Models/RegisterViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace WebApplication3.Models
+{
+    public class RegisterViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string RePassword { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string ContactNumber { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string Branch { get; set; }
+        public string AccountType { get; set; }
+        public string AccountNumber { get; set; }
+
+        public string Pin { get; set; }
+
+        public string SecurityQuestion1 { get; set; }
+        public string SecurityQuestion2 { get; set; }
+        public string Answer1 { get; set; }
+        public string Answer2 { get; set; }
+    }
+}
